7ec64454f19544c1e5a3f3723d796381052db5ab
Finn Buick <82785441+FinnUpstreet@users.noreply.github.com>
2022-08-10 12:20:26 +1000

    code example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+interface User {
+  id: number;
+  name: string;
+  age: number;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([
+    { id: 1, name: 'John', age: 18 },
+    { id: 2, name: 'Mike', age: 36 },
+    { id: 3, name: 'Sally', age: 28 },
+    { id: 4, name: 'Bill', age: 53 },
+  ]);
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+  const incrementAge = (id: number) => {
+    setUsers((currUsers) => {
+      return currUsers.map((user) => {
+        if (user.id === id) {
+          return { ...user, age: user.age + 1 };
+        }
+        return user;
+      });
+    });
+  };
+
+  const selectUser = (id: number) => {
+    const user = users.find((user) => user.id === id);
+    if (user) {
+      setSelectedUser(user);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <h3>Selected User: {selectedUser === null ? 'None' : `${selectedUser.name} is ${selectedUser.age} years old`}</h3>
+      {users.map((user) => (
+        <div
+          key={user.id}
+          style={{ display: 'grid', gridTemplateColumns: '1fr auto auto', gap: '0.25rem', marginBottom: '0.5rem' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {user.name} is {user.age} years old
+          <button onClick={() => incrementAge(user.id)}>Increment Age</button>
+          <button onClick={() => selectUser(user.id)}>Select</button>
+        </div>
+      ))}
+    </>
   );
 }
 
